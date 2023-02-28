28e6c6532866d2fb944742bdeff9052120ad646e
Evgeny <ekolodenets@yahoo.com>
2023-02-28 19:35:10 +0100

    making CRUD methods

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create/', TaskCreate.as_view(), name='create'),
+    path('edit/<int:pk>/', TaskUpdate.as_view(), name='edit'),
+    path('delete/<int:pk>/', TaskDelete.as_view(), name='delete'),
+
 ]
